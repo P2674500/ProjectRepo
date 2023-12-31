2d15b1c7ab08a43b9364bc993d03121dcd945ded
P2674500 <P2674500@my365.dmu.ac.uk>
2023-12-31 20:26:11 +0000

    Tested show and movie class stuff

--- Testing2/tstShowAndMovie.cs
+++ Testing2/tstShowAndMovie.cs
@@ -1,6 +1,7 @@
 ﻿using ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.ComponentModel;
 
 namespace Testing2
 {
@@ -15,5 +16,75 @@ namespace Testing2
             //test to see that it exists
             Assert.IsNotNull(AShowOrMovie);
         }
+
+        [TestMethod]
+        public void TitlePropertyOK()
+        {
+            clsShowAndMovie AShowOrMovie = new clsShowAndMovie();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AShowOrMovie.Title = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AShowOrMovie.Title, TestData);
+        }
+
+        [TestMethod]
+        public void ReleaseDatePropertyOK()
+        {
+            clsShowAndMovie AShowOrMovie = new clsShowAndMovie();
+            DateTime TestData = DateTime.Now.Date;
+            AShowOrMovie.ReleaseDate = TestData;
+            Assert.AreEqual(AShowOrMovie.ReleaseDate, TestData);
+        }
+
+        [TestMethod]
+        public void RunTimePropertyOK()
+        {
+            clsShowAndMovie AShowOrMovie = new clsShowAndMovie();
+            double hours = 1.0;
+            double minutes = 30.0;
+            double seconds = 20.0;
+            TimeSpan TestData = TimeSpan.FromHours(hours + minutes / 60 + seconds / 3600);
+            AShowOrMovie.RunTime = TestData;
+            Assert.AreEqual(AShowOrMovie.RunTime, TestData);
+        }
+
+        [TestMethod]
+        public void DecriptionPropertyOK()
+        {
+            clsShowAndMovie AShowOrMovie = new clsShowAndMovie();
+            string TestData = "An exciting film.";
+            AShowOrMovie.Description = TestData;
+            Assert.AreEqual(AShowOrMovie.Description, TestData);
+        }
+
+        [TestMethod]
+        public void GenrePropertyOK()
+        {
+            clsShowAndMovie AShowOrMovie = new clsShowAndMovie();
+            string TestData = "Action";
+            AShowOrMovie.Genre = TestData;
+            Assert.AreEqual(AShowOrMovie.Genre, TestData);
+        }
+
+        [TestMethod]
+        public void RatingPropertyOK()
+        {
+            clsShowAndMovie AShowOrMovie = new clsShowAndMovie();
+            double TestData = 4.5;
+            AShowOrMovie.Rating = TestData;
+            Assert.AreEqual(AShowOrMovie.Rating, TestData);
+        }
+
+        [TestMethod]
+        public void IsShowPropertyOK()
+        {
+            clsShowAndMovie AShowOrMovie = new clsShowAndMovie();
+            Boolean TestData = true;
+            AShowOrMovie.IsShow = TestData;
+            Assert.AreEqual(AShowOrMovie.IsShow, TestData);
+        }
+
     }
 }
